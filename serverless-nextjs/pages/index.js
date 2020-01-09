7cd7c39f5920807f2788a5b449ebfa5403379dc0
Thales <thales@carteiraglobal.com>
2020-01-09 14:01:38 -0300

    Creating First Layout

--- serverless-nextjs/pages/index.js
+++ serverless-nextjs/pages/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
-import Link from 'next/link';
+import Layout from '../components/MyLayout';
 
-const Home = () => (
-  <div>
-    <Link title="About Page" href="/about">
-      <a>About Page</a>
-    </Link>
-
-    <p>Hello Next.js</p>
-  </div>
-)
-
-export default Home
+export default function Index() {
+  return (
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  );
+}
